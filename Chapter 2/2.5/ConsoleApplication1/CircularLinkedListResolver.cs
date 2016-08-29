0042cfc03da6bffe8b926998cb7b905450213a49
and85 <andreytkach85@gmai.com>
2016-08-29 17:05:36 +0200

    solution for 2.5

---
+++ Chapter 2/2.5/ConsoleApplication1/CircularLinkedListResolver.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class CircularLinkedListResolver<T>
+    {
+        public Node<T> First { get; set; }
+        public Node<T> Last { get; set; }
+
+        public void Add(Node<T> node)
+        {
+            if (First == null)
+            {
+                First = node;
+                Last = node;
+                
+                return;
+            }
+
+            Last.Next = node;
+            Last = node;
+        }
+
+        // Requires an additional storage
+        public Node<T> FindLoopWithAdditionalStorage()
+        {
+            HashSet<Node<T>> uniqueItems = new HashSet<Node<T>>();
+
+            Node<T> curNode = First;
+            while (curNode.Next != null)
+            {
+                if (!uniqueItems.Add(curNode))
+                {
+                    return curNode;
+                }
+
+                curNode = curNode.Next;
+            }
+
+            return Last;
+        }
+
+        // From the book
+        public Node<T> FindLoop()
+        {            
+            Node<T> head = First;    
+            
+            Node<T> n1 = head; 
+            Node<T> n2 = head; 
+   
+            // Find meeting point 
+            while (n2.Next != null) 
+            { 
+                n1 = n1.Next; 
+                n2 = n2.Next.Next; 
+                if (n1 == n2)  
+                    break; 
+            } 
+ 
+            // Error check - there is no meeting point, and therefore no loop 
+            if (n2.Next == null) 
+                return null; 
+ 
+            /* Move n1 to Head. Keep n2 at Meeting Point.  Each are k steps 
+            /* from the Loop Start. If they move at the same pace, they must 
+            * meet at Loop Start. */ 
+            n1 = head; 
+            while (n1 != n2) 
+            { 
+                n1 = n1.Next; 
+                n2 = n2.Next; 
+            } 
+            // Now n2 points to the start of the loop. 
+            return n2; 
+        }
+    }
+}
+
---
+++ Chapter 2/2.5/ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        /*
+         Given a circular linked list, implement an algorithm which returns node at the beginning of the loop 
+         * 
+         * DEFINITION 
+         * Circular linked list: A (corrupt) linked list in which a node’s next pointer points to an earlier node, 
+         * so as to make a loop in the linked list 
+         * 
+         * EXAMPLE 
+         * Input: A -> B -> C -> D -> E -> C [the same C as earlier] 
+         * Output: C
+        */
+        static void Main(string[] args)
+        {
+            var nodeA = new Node<string>();
+            var nodeB = new Node<string>();
+            var nodeC = new Node<string>();
+            var nodeD = new Node<string>();
+            var nodeE = new Node<string>();
+
+            nodeA.Value = "A";
+            nodeB.Value = "B";
+            nodeC.Value = "C";
+            nodeD.Value = "D";
+            nodeE.Value = "E";
+
+            var linkedList = new CircularLinkedListResolver<string>();
+            linkedList.Add(nodeA);
+            linkedList.Add(nodeB);
+            linkedList.Add(nodeC);
+            linkedList.Add(nodeD);
+            linkedList.Add(nodeE);
+            linkedList.Add(nodeC);
+
+            Node<string> nodeX = linkedList.FindLoop();
+            Console.WriteLine(nodeX.Value);
+
+#if DEBUG
+            Console.WriteLine("END");
+            Console.ReadLine();
+#endif
+        }
+    }
+}
+
