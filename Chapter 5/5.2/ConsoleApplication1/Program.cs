312f2787da5e03c79f9a0fe28f368a1fbfb03394
and85 <andreytkach85@gmai.com>
2016-09-28 21:43:11 +0200

    Added 5.2 template

---
+++ Chapter 5/5.2/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //Given a(decimal - e.g. 3.72) number that is passed in as a string, print the binary representation   
+        //If the number can not be represented accurately in binary, print “ERROR”
+        static void Main(string[] args)
+        {
+            decimal value = (decimal)3.72;
+            int[] bits = decimal.GetBits(value);
+            string str = string.Join(string.Empty, bits);
+            Console.WriteLine(str);
+
+            Console.WriteLine("END");
+            Console.ReadLine();
+        }
+    }
+}
