00d952a0426eed154a8a07188a9a9635661f4a77
and85 <andreytkach85@gmai.com>
2016-11-30 22:33:24 +0100

    8.1

---
+++ Chapter 8/8.1/8.1/8.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1
+{
+    // Write a method to generate the nth Fibonacci number 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = Fibonacci(n);
+
+            Console.WriteLine(result);
+#if DEBUG
+            Console.WriteLine("END");
+            Console.ReadLine();
+#endif
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 1;
+
+            return Fibonacci(n - 2) + Fibonacci(n - 1);
+        }
+    }
+}
