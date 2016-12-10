f18c5e52a8e2f0c4fb0dc6a3261bbfe888c0d266
and85 <andreytkach85@gmai.com>
2016-12-10 14:37:00 +0100

    Get all permutations of a string

---
+++ Chapter 8/8.4/AllPermutations/AllPermutations/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string perm in GetPerms("abc"))
+            {
+                Console.WriteLine(perm);
+            }
+
+#if DEBUG
+            Console.WriteLine("END");
+            Console.ReadLine();
+#endif
+        }
+
+        public static List<string> GetPerms(string s)
+        {
+            var permutations = new List<string>();
+            if (s == null)
+            {
+                return null;
+            }
+            else if (s.Length == 0)
+            {
+                permutations.Add("");
+                return permutations;
+            }
+            char first = s[0];
+            var remainder = s.Substring(1);
+            var words = GetPerms(remainder);
+            foreach (string word in words)
+            {
+                for (int j = 0; j <= word.Length; j++)
+                {
+                    permutations.Add(InsertCharAt(word, first, j));
+                }
+            }
+            return permutations;
+        }
+
+        public static String InsertCharAt(String word, char c, int i)
+        {
+            string start = word.Substring(0, i);
+            var end = word.Substring(i);
+            return start + c + end;
+        }
+    }
+}
+
