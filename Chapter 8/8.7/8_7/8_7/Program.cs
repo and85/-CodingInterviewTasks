e686e28f7d33a2302a7ba893fb3adcdae3acd0c9
and85 <andreytkach85@gmai.com>
2016-12-12 19:24:55 +0100

    8.7

---
+++ Chapter 8/8.7/8_7/8_7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(makeChange(100, 25));
+            Console.ReadLine();
+        }
+
+        public static int makeChange(int n, int denom)
+        {
+            int next_denom = 0;
+            switch (denom)
+            { 
+                case 25: 
+                    next_denom = 10;
+                    break;
+                case 10: 
+                    next_denom = 5;
+                    break;
+                case 5: 
+                    next_denom = 1;
+                    break;
+                case 1: 
+                    return 1;
+            } 
+
+            int ways = 0; 
+            for (int i = 0; i* denom <= n; i++)
+                ways += makeChange(n - i* denom, next_denom);
+
+            return ways; 
+        } 
+    }
+}
